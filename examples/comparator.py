d7d67c5d0f173e73b613ab7e017ee80a690510b5
Tony DiCola <tony@tonydicola.com>
2016-02-09 11:37:02 +0000

    Clarify voltage range for gain in examples.

--- examples/comparator.py
+++ examples/comparator.py
@@ -20,12 +20,12 @@ adc = Adafruit_ADS1x15.ADS1115()
 
 # Choose a gain of 1 for reading voltages from 0 to 4.09V.
 # Or pick a different gain to change the range of voltages that are read:
-#  - 2/3 = 6.144V
-#  -   1 = 4.096V
-#  -   2 = 2.048V
-#  -   4 = 1.024V
-#  -   8 = 0.512V
-#  -  16 = 0.256V
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
 # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
 GAIN = 1
 
